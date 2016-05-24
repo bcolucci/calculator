5330e72877fdb1c59d329b2d8a9f633f54cb1e64
Brice Colucci <bcolucci@wuha.io>
2016-05-24 17:01:21 +0200

    wip

--- version/brice.js
+++ version/brice.js
@@ -6,11 +6,36 @@ const engine = () => {
 
   const isNumber = x => String(Number(x)) === x;
 
+  const containsArray = arr => {
+    for (var i = 0; i < arr.length; i++) {
+      if (Array.isArray(arr[i]))
+        return true;
+    }
+    return false;
+  };
+
+  const calculateFlatArray = arr => {
+    let priorIndex = ((arr.indexOf('x') + 1) || (arr.indexOf('/') + 1)) - 1;
+    if (priorIndex === -1)
+      return arr.reduce((acc, c) => {
+        const accIsNumber = typeof(acc) === 'number';
+        if (typeof(c) === 'number')
+      }, 0);
+    //TODO
+  };
+
+  const calculate = arr => {
+    //TODO
+    return 0;
+  };
+
   const instance = function () {
 
     this.cmdBuffer = [];
 
     this.pushCmd = function (cmd) {
+      if (cmd === '=')
+        return calculate(this.toDeepArray());
       if (cmd === 'C') {
         this.cmdBuffer.splice(0, this.cmdBuffer.length);
         return this;
@@ -39,7 +64,25 @@ const engine = () => {
       return this.cmdBuffer;
     };
 
-    this.toDeepArray = R.identity;
+    this.toDeepArray = function (chars) {
+      chars = chars || this.cmdBuffer.slice();
+      const arr = [];
+      const parenthesis = '()'.split('');
+      while (chars.length > 0) {
+        const c = chars.shift();
+        if (parenthesis.indexOf(c) === -1) {
+          arr.push(c);
+          continue;
+        }
+        if (c === parenthesis[0]) {
+          arr.push(this.toDeepArray(chars));
+          continue;
+        }
+        if (c === parenthesis[1])
+          break;
+      }
+      return arr;
+    }
 
   };
 
